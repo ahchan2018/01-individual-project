7a422263bd96fb62990aa4532042517f90ef380c
ahchan2018 <36979652+ahchan2018@users.noreply.github.com>
2018-11-21 13:13:49 +0800

    Update kaggle.py

--- kaggle.py
+++ kaggle.py
@@ -7,13 +7,15 @@ pd.set_option('display.max_columns', None)
 
 
 def readData():
-    df_train = pd.read_csv('train.csv')
+    #read train set data
+    df_train = pd.read_csv('train.csv') 
     df_train['train'] = 1
-    df_test = pd.read_csv('test.csv')
+    #read test set data
+    df_test = pd.read_csv('test.csv') 
     df_test_y = pd.read_csv('test_y.csv')
     df_test['time'] = df_test_y['time']
     df_test['train'] = 0
-    df = pd.concat([df_train, df_test], sort=False, ignore_index=True)
+    df = pd.concat([df_train, df_test], sort=False, ignore_index=True) #concatenate the training set data and test set data
     return df
 
 
@@ -23,21 +25,30 @@ def onehot(df, col):
 
 
 def trainMlp(x_data, y_data, x_test, y_test):
+    #split the train set and validation set
     train_data, val_data, train_label, val_label = train_test_split(x_data, y_data, test_size=0.1, random_state=10)
 
     print((x_test.shape))
     print((y_test.shape))
     print((train_data.shape))
     print((train_label.shape))
-    H_DIM = 4
+    #set the dimension for hidden layer
+    H_DIM = 4 
+    
+    #number of features in train dataset
     col = len(train_data[0])
+    
+    #initialize tf
     x = tf.placeholder(float, [None, col])
     y = tf.placeholder(float, [None, 1])
 
+    #initialize input layer
     w1 = tf.Variable(tf.random_normal([col, H_DIM]))
     b1 = tf.Variable(tf.constant(0.0, shape=[H_DIM]))
     # w2 = tf.Variable(tf.random_normal([H_DIM, H_DIM]))
     # b2 = tf.Variable(tf.constant(0.0, shape=[H_DIM]))
+    
+    #initialize the output layer
     w3 = tf.Variable(tf.random_normal([H_DIM, 1]))
     b3 = tf.Variable(tf.constant(0.0, shape=[1]))
     hidden_layer = tf.nn.tanh(tf.matmul(x, w1) + b1)
@@ -45,21 +56,25 @@ def trainMlp(x_data, y_data, x_test, y_test):
     # hidden_layer2 = tf.nn.tanh(tf.matmul(hidden_layer, w2) + b2)
     yhat = (tf.matmul(hidden_layer, w3) + b3)
 
-    loss = tf.reduce_mean(tf.abs((yhat) - y))
-    loss2 = tf.reduce_mean(tf.square(tf.abs(yhat) - y))
-    optimizer = tf.train.AdamOptimizer(0.05).minimize(loss)
+    loss = tf.reduce_mean(tf.abs((yhat) - y)) #use mean average error as training loss
+    loss2 = tf.reduce_mean(tf.square(tf.abs(yhat) - y)) #use mean square error as validation loss
+    optimizer = tf.train.AdamOptimizer(0.05).minimize(loss) #use Adam optimizer
 
-    init = tf.global_variables_initializer()
+    init = tf.global_variables_initializer() #initialize all the data
     sess = tf.Session()
     sess.run(init)
 
+    #train 400,000 process steps
     for step in range(0, 400000):
         sess.run(optimizer, feed_dict={x: train_data, y: train_label})
         if (step % 4000 == 0):
+            #observe the loss2 for each session, pick the best loss2, write the csv
             print(sess.run(loss2, feed_dict={x: train_data, y: train_label}))
             print(sess.run(loss2, feed_dict={x: train_data, y: train_label}))
             print(sess.run(loss2, feed_dict={x: val_data, y: val_label}))
             print(sess.run(loss2, feed_dict={x: x_test, y: y_test}))
+            
+            #write the ans csv for testset  
             ans_y = sess.run(yhat, feed_dict={x: x_test, y: train_label})
             ans = pd.DataFrame(columns=['Id', 'time'])
             ans['Id'] = [i for i in range(0, 100)]
@@ -70,10 +85,10 @@ def trainMlp(x_data, y_data, x_test, y_test):
 def getCleanData(orgin_df):
     df = pd.DataFrame(orgin_df)
 
-    # do onehot
+    # do onehot for 'penalty'
     df = df.merge(onehot(orgin_df, 'penalty'), left_index=True, right_index=True)
 
-    # n_jobs
+    # n_jobs for x=-1
     def fun(x):
         if (x == -1):
             return 100
@@ -85,7 +100,7 @@ def getCleanData(orgin_df):
 
 
 def doFeatureEngineeriing(df):
-    # normalization
+    # normalization features
     columns = df.columns.tolist();
     columns.remove('id')
     columns.remove('penalty')
@@ -101,6 +116,7 @@ def doFeatureEngineeriing(df):
     x_test = df_test[columns]
     y_test = df_test['time']
 
+    #convert to np.array format
     x_data = np.array(x_train, dtype=np.float32)
     y_data = np.array(y_train, dtype=np.float32)
 
